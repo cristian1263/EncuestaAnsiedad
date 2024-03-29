974c78f5aeace53715210a06b32192381ffb0fa8
Luis Gordon <luisdgordon@gmail.com>
2024-03-29 02:12:44 -0500

    Validados campos de nombre e email

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $('#btnEnviar').click(function () {
         var faltanRespuestas = ""; // Variable para almacenar las opciones que faltan responder
-    
+
         // Validar cada opción
         for (var i = 1; i <= 21; i++) {
             if (!$("input[name='respuesta" + i + "']:checked").val()) {
                 // Si la opción no está seleccionada, agregarla a la lista de faltanRespuestas
                 faltanRespuestas += "respuesta" + i + ", ";
-    
+
                 // Agregar la clase al div que contiene la respuesta faltante
                 $("#divRespuesta" + i).addClass("respuesta-faltante");
             } else {
@@ -15,7 +15,34 @@ $(document).ready(function () {
                 $("#divRespuesta" + i).removeClass("respuesta-faltante");
             }
         }
-    
+
+        // Validar el checkbox checkTerms
+        if (!$('#checkTerms').is(':checked')) {
+            $("#check").addClass("respuesta-faltante");
+            alert("Debes aceptar el tratamiento de datos");
+           
+        }else{
+            $("#check").removeClass("respuesta-faltante");
+        }
+
+        // Validar el campo de texto Nombreinput
+        if (!$('#Nombreinput').val()) {
+            // Si el campo de texto está vacío, agregarlo a la lista de faltanRespuestas
+            faltanRespuestas += "Nombreinput, ";
+            $("#nombre").addClass("respuesta-faltante");
+        }else{
+            $("#nombre").removeClass("respuesta-faltante");
+        }
+
+        // Validar el campo de correo electrónico emailInput
+        if (!$('#emailInput').val()) {
+            // Si el campo de texto está vacío, agregarlo a la lista de faltanRespuestas
+            faltanRespuestas += "emailInput, ";
+            $("#email").addClass("respuesta-faltante");
+        }else{
+            $("#email").removeClass("respuesta-faltante");
+        }
+
         // Verificar si faltan opciones por responder
         if (faltanRespuestas !== "") {
             // Si hay opciones que faltan responder, mostrar un mensaje de advertencia
@@ -23,13 +50,13 @@ $(document).ready(function () {
         } else {
             // Si todas las opciones están seleccionadas, proceder con la suma
             var sumaRespuestas = 0;
-    
+
             // Realizar la suma
             for (var i = 1; i <= 21; i++) {
                 var respuesta = parseInt($("input[name='respuesta" + i + "']:checked").val());
                 sumaRespuestas += respuesta;
             }
-            
+
             // Determinar el mensaje de acuerdo a la suma
             var mensaje = "";
             if (sumaRespuestas >= 0 && sumaRespuestas <= 21) {
@@ -42,9 +69,4 @@ $(document).ready(function () {
             $('#resultado').html('</br><div class="alert alert-info" role="alert">Su resultado es: ' + sumaRespuestas + '<br>' + mensaje + '</div>');
         }
     });
-    
-            
-
-        
-  
 });
