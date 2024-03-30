60f06373e10be1f3e2151f8b7cfb9508755bdb08
cristian <cristian.camilo1560@gmail.com>
2024-03-29 22:17:08 -0500

    Conexion al firebase realizada

--- index.js
+++ index.js
@@ -1,99 +1,104 @@
+//configuracion de firebase
+var firebaseConfig = {
+	apiKey: 'AIzaSyDkNPCbeZfWtNYsRCg6DxYfxnpYF8mRhwc',
+	authDomain: 'encuestaansiedad-1b8be.firebaseapp.com',
+	projectId: 'encuestaansiedad-1b8be',
+	storageBucket: 'encuestaansiedad-1b8be.appspot.com',
+	messagingSenderId: '780069763178',
+	appId: '1:780069763178:web:ba8a14a1cda72e775fb213',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
 $(document).ready(function () {
-    $('#btnEnviar').click(function () {
-        var faltanRespuestas = ""; 
+	$('#btnEnviar').click(function () {
+		var faltanRespuestas = '';
 
-        for (var i = 1; i <= 21; i++) {
-            if (!$("input[name='respuesta" + i + "']:checked").val()) {
-            
-                faltanRespuestas += "respuesta" + i + ", ";
+		for (var i = 1; i <= 21; i++) {
+			if (!$("input[name='respuesta" + i + "']:checked").val()) {
+				faltanRespuestas += 'respuesta' + i + ', ';
 
-                $("#divRespuesta" + i).addClass("respuesta-faltante");
-            } else {
-                $("#divRespuesta" + i).removeClass("respuesta-faltante");
-            }
-        }
+				$('#divRespuesta' + i).addClass('respuesta-faltante');
+			} else {
+				$('#divRespuesta' + i).removeClass('respuesta-faltante');
+			}
+		}
 
-        if (!$('#checkTerms').is(':checked')) {
-            $("#check").addClass("respuesta-faltante");
-            faltanRespuestas += "checkTerms, "; 
-            alert("Debes aceptar el tratamiento de datos");
-           
-        }else{
-            $("#check").removeClass("respuesta-faltante");
-        }
+		if (!$('#checkTerms').is(':checked')) {
+			$('#check').addClass('respuesta-faltante');
+			faltanRespuestas += 'checkTerms, ';
+			alert('Debes aceptar el tratamiento de datos');
+		} else {
+			$('#check').removeClass('respuesta-faltante');
+		}
 
-        if (!$('#Nombreinput').val()) {
-            faltanRespuestas += "Nombreinput, ";
-            $("#nombre").addClass("respuesta-faltante");
-        }else{
-            $("#nombre").removeClass("respuesta-faltante");
-        }
+		if (!$('#Nombreinput').val()) {
+			faltanRespuestas += 'Nombreinput, ';
+			$('#nombre').addClass('respuesta-faltante');
+		} else {
+			$('#nombre').removeClass('respuesta-faltante');
+		}
 
-        if (!$('#emailInput').val()) {
-            faltanRespuestas += "emailInput, ";
-            $("#email").addClass("respuesta-faltante");
-        }else{
-            $("#email").removeClass("respuesta-faltante");
-        }
+		if (!$('#emailInput').val()) {
+			faltanRespuestas += 'emailInput, ';
+			$('#email').addClass('respuesta-faltante');
+		} else {
+			$('#email').removeClass('respuesta-faltante');
+		}
 
-        if (faltanRespuestas == "checkTerms, ") {
-          return "";
-        } else if(faltanRespuestas !== ""){
-              alert("Complete todos los campos");
-        }else {
-            var sumaRespuestas = 0;
+		if (faltanRespuestas == 'checkTerms, ') {
+			return '';
+		} else if (faltanRespuestas !== '') {
+			alert('Complete todos los campos');
+		} else {
+			var sumaRespuestas = 0;
 
-            for (var i = 1; i <= 21; i++) {
-                var respuesta = parseInt($("input[name='respuesta" + i + "']:checked").val());
-                sumaRespuestas += respuesta;
-            }
+			for (var i = 1; i <= 21; i++) {
+				var respuesta = parseInt($("input[name='respuesta" + i + "']:checked").val());
+				sumaRespuestas += respuesta;
+			}
 
-            var mensaje = "";
-            if (sumaRespuestas >= 0 && sumaRespuestas <= 21) {
-                mensaje = "Ansiedad muy baja";
-            } else if (sumaRespuestas >= 22 && sumaRespuestas <= 35) {
-                mensaje = "Ansiedad moderada";
-            } else {
-                mensaje = "Ansiedad severa";
-            }
-            alert("Respuestas guardadas, muchas gracias por participar");
-          /*   $('#resultado').html('</br><div class="alert alert-info" role="alert">Su resultado es: ' + sumaRespuestas + '<br>' + mensaje + '</div>');*/
-        
+			var mensaje = '';
+			if (sumaRespuestas >= 0 && sumaRespuestas <= 21) {
+				mensaje = 'Ansiedad muy baja';
+			} else if (sumaRespuestas >= 22 && sumaRespuestas <= 35) {
+				mensaje = 'Ansiedad moderada';
+			} else {
+				mensaje = 'Ansiedad severa';
+			}
+			alert('Respuestas guardadas, muchas gracias por participar');
+			/*   $('#resultado').html('</br><div class="alert alert-info" role="alert">Su resultado es: ' + sumaRespuestas + '<br>' + mensaje + '</div>');*/
 
-             /*--------Creación del Objeto JSON--------*/ 
+			/*--------Creación del Objeto JSON--------*/
 
-            var nombre = $("#Nombreinput").val();
-            var email = $("#emailInput").val();
-            var respuestas = {};
-    
-            $("[id^='divRespuesta']").each(function (index, element) {
-                var respuesta = $(element).find("input:checked").val();
-                respuestas["Pregunta " + (index + 1)] = {
-                    "Respuesta": respuesta
-                };
-            });
-    
-            var terminosAceptados = $("#checkTerms").is(":checked");
-    
-            if (terminosAceptados) {
-    
-                var encuestaData = {
-                    "Nombre": nombre,
-                    "Email": email,
-                    "Respuestas": respuestas,
-                    "TerminosAceptados": terminosAceptados
-                };
-        
-                var encuestaJSON = JSON.stringify(encuestaData);
-        
-                console.log(encuestaJSON);
-              }
-           
-       
-        
-        
-        }
-    });
+			var nombre = $('#Nombreinput').val();
+			var email = $('#emailInput').val();
+			var respuestas = {};
+
+			$("[id^='divRespuesta']").each(function (index, element) {
+				var respuesta = $(element).find('input:checked').val();
+				respuestas['Pregunta ' + (index + 1)] = {
+					Respuesta: respuesta,
+				};
+			});
+
+			var terminosAceptados = $('#checkTerms').is(':checked');
+
+			if (terminosAceptados) {
+				var encuestaData = {
+					Nombre: nombre,
+					Email: email,
+					Respuestas: respuestas,
+					TerminosAceptados: terminosAceptados,
+				};
+
+				var encuestaJSON = JSON.stringify(encuestaData);
+
+				console.log(encuestaJSON);
+			}
+		}
+	});
 });
-
-
