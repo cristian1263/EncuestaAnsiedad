2d04d6d1584268a14b69e3cd9bd6230a6d972a44
Luis Gordon <luisdgordon@gmail.com>
2024-04-03 21:30:32 -0500

    Correcciones de estructura

--- index.js
+++ index.js
@@ -30,21 +30,25 @@ $(document).ready(function () {
   $('#btnEnviar').click(function () {
     var faltantes = respuestasFaltantes();
     calcAnsiedad(faltantes);
-   });
+  });
 });
 
-    function respuestasFaltantes() {
-      var faltanRespuestas = '';
 
-      for (var i = 1; i <= 21; i++) {
-        if (!$("input[name='respuesta" + i + "']:checked").val()) {
-          faltanRespuestas += 'respuesta' + i + ', ';
 
-          $('#divRespuesta' + i).css('background-color', '#ffcccc');
-        } else {
-          $('#divRespuesta' + i).css('background-color', '');
-        }
-      }
+/*--------Funciones--------*/
+
+function respuestasFaltantes() {
+  var faltanRespuestas = '';
+
+  for (var i = 1; i <= 21; i++) {
+    if (!$("input[name='respuesta" + i + "']:checked").val()) {
+      faltanRespuestas += 'respuesta' + i + ', ';
+
+      $('#divRespuesta' + i).css('background-color', '#ffcccc');
+    } else {
+      $('#divRespuesta' + i).css('background-color', '');
+    }
+  }
 
   if (!$('#checkTerms').is(':checked')) {
     $('#checkTerms').css('background-color', '#ffcccc');
@@ -55,133 +59,133 @@ $(document).ready(function () {
     $('#checkTerms').css('background-color', '');
   }
 
- if (!$('#Nombreinput').val()) {
-      faltanRespuestas += 'Nombreinput, ';
-      $('#Nombreinput').css('background-color', '#ffcccc');
-    } else {
-      $('#Nombreinput').css('background-color', '');
-    }
+  if (!$('#Nombreinput').val()) {
+    faltanRespuestas += 'Nombreinput, ';
+    $('#Nombreinput').css('background-color', '#ffcccc');
+  } else {
+    $('#Nombreinput').css('background-color', '');
+  }
 
-    var email = $('#emailInput').val();
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var email = $('#emailInput').val();
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-    if (!$('#emailInput').val() || !emailRegex.test($('#emailInput').val())) {
-      faltanRespuestas += 'emailInput, ';
-      $('#emailInput').css('background-color', '#ffcccc');
-      if (!emailRegex.test($('#emailInput').val()) && $('#emailInput').val() && faltanRespuestas == 'emailInput, ') {
-        MSJERROR4();
-        return "";
-      }  
-    } else {
-      $('#emailInput').css('background-color', '');
-    }
-      return faltanRespuestas;
-    }
+  if (!$('#emailInput').val() || !emailRegex.test($('#emailInput').val())) {
+    faltanRespuestas += 'emailInput, ';
+    $('#emailInput').css('background-color', '#ffcccc');
+    if (!emailRegex.test($('#emailInput').val()) && $('#emailInput').val() && faltanRespuestas == 'emailInput, ') {
+      MSJERROR4();
+      return "";
+    }  
+  } else {
+    $('#emailInput').css('background-color', '');
+  }
+  return faltanRespuestas;
+}
 
 function calcAnsiedad(faltanRespuestas) {
   if (faltanRespuestas == 'checkTerms, ') {
-      return '';
-    } else if (faltanRespuestas !== '') {
-      MSJERROR1();
-      //alert('Complete todos los campos');
-    } else {
-      var sumaRespuestas = 0;
+    return '';
+  } else if (faltanRespuestas !== '') {
+    MSJERROR1();
+    //alert('Complete todos los campos');
+  } else {
+    var sumaRespuestas = 0;
 
-      for (var i = 1; i <= 21; i++) {
-        var respuesta = parseInt($("input[name='respuesta" + i + "']:checked").val());
-        sumaRespuestas += respuesta;
-      }
-
-      var mensaje = '';
-      if (sumaRespuestas >= 0 && sumaRespuestas <= 21) {
-        mensaje = 'Ansiedad muy baja';
-      } else if (sumaRespuestas >= 22 && sumaRespuestas <= 35) {
-        mensaje = 'Ansiedad moderada';
-      } else {
-        mensaje = 'Ansiedad severa';
-      }
-
-      parseInt(sumaRespuestas);
-      
-
-      var respuestaCalc = [
-        sumaRespuestas,
-        mensaje
-      ];
-
-      crearJson(respuestaCalc);
-      //MSJOK();
-      //alert('Respuestas guardadas, muchas gracias por participar');
-      /*   $('#resultado').html('</br><div class="alert alert-info" role="alert">Su resultado es: ' + sumaRespuestas + '<br>' + mensaje + '</div>');*/
-
-
-}
-  
-function crearJson() {
-      
-      var nombre = $('#Nombreinput').val();
-      var email = $('#emailInput').val();
-      var respuesta1 = parseInt($("input[name='respuesta1']:checked").val());
-      var respuesta2 = parseInt($("input[name='respuesta2']:checked").val());
-      var respuesta3 = parseInt($("input[name='respuesta3']:checked").val());
-      var respuesta4 = parseInt($("input[name='respuesta4']:checked").val());
-      var respuesta5 = parseInt($("input[name='respuesta5']:checked").val());
-      var respuesta6 = parseInt($("input[name='respuesta6']:checked").val());
-      var respuesta7 = parseInt($("input[name='respuesta7']:checked").val());
-      var respuesta8 = parseInt($("input[name='respuesta8']:checked").val());
-      var respuesta9 = parseInt($("input[name='respuesta9']:checked").val());
-      var respuesta10 = parseInt($("input[name='respuesta10']:checked").val());
-      var respuesta11 = parseInt($("input[name='respuesta11']:checked").val());
-      var respuesta12 = parseInt($("input[name='respuesta12']:checked").val());
-      var respuesta13 = parseInt($("input[name='respuesta13']:checked").val());
-      var respuesta14 = parseInt($("input[name='respuesta14']:checked").val());
-      var respuesta15 = parseInt($("input[name='respuesta15']:checked").val());
-      var respuesta16 = parseInt($("input[name='respuesta16']:checked").val());
-      var respuesta17 = parseInt($("input[name='respuesta17']:checked").val());
-      var respuesta18 = parseInt($("input[name='respuesta18']:checked").val());
-      var respuesta19 = parseInt($("input[name='respuesta19']:checked").val());
-      var respuesta20 = parseInt($("input[name='respuesta20']:checked").val());
-      var respuesta21 = parseInt($("input[name='respuesta21']:checked").val());
-      var terminosAceptados = $('#checkTerms').is(':checked');
-
-      if (terminosAceptados) {
-        var encuestaData = {
-          Nombre: nombre,
-          Email: email,
-          Respuesta1: respuesta1,
-          Respuesta2: respuesta2,
-          Respuesta3: respuesta3,
-          Respuesta4: respuesta4,
-          Respuesta5: respuesta5,
-          Respuesta6: respuesta6,
-          Respuesta7: respuesta7,
-          Respuesta8: respuesta8,
-          Respuesta9: respuesta9,
-          Respuesta10: respuesta10,
-          Respuesta11: respuesta11,
-          Respuesta12: respuesta12,
-          Respuesta13: respuesta13,
-          Respuesta14: respuesta14,
-          Respuesta15: respuesta15,
-          Respuesta16: respuesta16,
-          Respuesta17: respuesta17,
-          Respuesta18: respuesta18,
-          Respuesta19: respuesta19,
-          Respuesta20: respuesta20,
-          Respuesta21: respuesta21,
-          Total: respuestaCalc[0],
-          Resultado: respuestaCalc[1],
-          TerminosAceptados: terminosAceptados,
-        };
-
-        var encuestaJSON = JSON.stringify(encuestaData);
-
-        console.log("OK");
-
-        SaveUser(encuestaData);
-      }
+    for (var i = 1; i <= 21; i++) {
+      var respuesta = parseInt($("input[name='respuesta" + i + "']:checked").val());
+      sumaRespuestas += respuesta;
     }
+
+    var mensaje = '';
+    if (sumaRespuestas >= 0 && sumaRespuestas <= 21) {
+      mensaje = 'Ansiedad muy baja';
+    } else if (sumaRespuestas >= 22 && sumaRespuestas <= 35) {
+      mensaje = 'Ansiedad moderada';
+    } else {
+      mensaje = 'Ansiedad severa';
+    }
+
+    parseInt(sumaRespuestas);
+
+
+    var respuestaCalc = [
+      sumaRespuestas,
+      mensaje
+    ];
+
+    crearJson(respuestaCalc);
+    //MSJOK();
+    //alert('Respuestas guardadas, muchas gracias por participar');
+    /*   $('#resultado').html('</br><div class="alert alert-info" role="alert">Su resultado es: ' + sumaRespuestas + '<br>' + mensaje + '</div>');*/
+
+
+  }
+
+  function crearJson() {
+
+    var nombre = $('#Nombreinput').val();
+    var email = $('#emailInput').val();
+    var respuesta1 = parseInt($("input[name='respuesta1']:checked").val());
+    var respuesta2 = parseInt($("input[name='respuesta2']:checked").val());
+    var respuesta3 = parseInt($("input[name='respuesta3']:checked").val());
+    var respuesta4 = parseInt($("input[name='respuesta4']:checked").val());
+    var respuesta5 = parseInt($("input[name='respuesta5']:checked").val());
+    var respuesta6 = parseInt($("input[name='respuesta6']:checked").val());
+    var respuesta7 = parseInt($("input[name='respuesta7']:checked").val());
+    var respuesta8 = parseInt($("input[name='respuesta8']:checked").val());
+    var respuesta9 = parseInt($("input[name='respuesta9']:checked").val());
+    var respuesta10 = parseInt($("input[name='respuesta10']:checked").val());
+    var respuesta11 = parseInt($("input[name='respuesta11']:checked").val());
+    var respuesta12 = parseInt($("input[name='respuesta12']:checked").val());
+    var respuesta13 = parseInt($("input[name='respuesta13']:checked").val());
+    var respuesta14 = parseInt($("input[name='respuesta14']:checked").val());
+    var respuesta15 = parseInt($("input[name='respuesta15']:checked").val());
+    var respuesta16 = parseInt($("input[name='respuesta16']:checked").val());
+    var respuesta17 = parseInt($("input[name='respuesta17']:checked").val());
+    var respuesta18 = parseInt($("input[name='respuesta18']:checked").val());
+    var respuesta19 = parseInt($("input[name='respuesta19']:checked").val());
+    var respuesta20 = parseInt($("input[name='respuesta20']:checked").val());
+    var respuesta21 = parseInt($("input[name='respuesta21']:checked").val());
+    var terminosAceptados = $('#checkTerms').is(':checked');
+
+    if (terminosAceptados) {
+      var encuestaData = {
+        Nombre: nombre,
+        Email: email,
+        Respuesta1: respuesta1,
+        Respuesta2: respuesta2,
+        Respuesta3: respuesta3,
+        Respuesta4: respuesta4,
+        Respuesta5: respuesta5,
+        Respuesta6: respuesta6,
+        Respuesta7: respuesta7,
+        Respuesta8: respuesta8,
+        Respuesta9: respuesta9,
+        Respuesta10: respuesta10,
+        Respuesta11: respuesta11,
+        Respuesta12: respuesta12,
+        Respuesta13: respuesta13,
+        Respuesta14: respuesta14,
+        Respuesta15: respuesta15,
+        Respuesta16: respuesta16,
+        Respuesta17: respuesta17,
+        Respuesta18: respuesta18,
+        Respuesta19: respuesta19,
+        Respuesta20: respuesta20,
+        Respuesta21: respuesta21,
+        Total: respuestaCalc[0],
+        Resultado: respuestaCalc[1],
+        TerminosAceptados: terminosAceptados,
+      };
+
+      var encuestaJSON = JSON.stringify(encuestaData);
+
+      console.log("OK");
+
+      SaveUser(encuestaData);
+    }
+  }
 
 }
 
